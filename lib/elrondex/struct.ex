8bdd029a718c4a864635d34b04d776c46bc324b9
Vlad Preda <vlad@fluxinternet.ro>
2021-11-21 15:17:34 +0200

    Adding boolean and decimal types to struct module

--- lib/elrondex/struct.ex
+++ lib/elrondex/struct.ex
@@ -1,4 +1,5 @@
 defmodule Elrondex.Struct do
+  require Decimal
   # TODO map to empty map
   def struct_load(struct, %{}, [], _version) do
     struct
@@ -23,6 +24,26 @@ defmodule Elrondex.Struct do
     case type do
       :integer ->
         struct_set_integer(
+          struct,
+          struct_field,
+          Map.get(payload, payload_field),
+          require,
+          since_version >= version,
+          default
+        )
+
+      :boolean ->
+        struct_set_boolean(
+          struct,
+          struct_field,
+          Map.get(payload, payload_field),
+          require,
+          since_version >= version,
+          default
+        )
+
+      :decimal ->
+        struct_set_decimal(
           struct,
           struct_field,
           Map.get(payload, payload_field),
@@ -97,6 +118,66 @@ defmodule Elrondex.Struct do
 
   def struct_set_integer(struct, key, nil, @require_yes, @since_match_yes, _default) do
     raise "missign value for key #{key} when load #{struct.__struct__} struct"
+  end
+
+  # struct, key, value, require, version_match, default
+  def struct_set_boolean(struct, key, value, _require, _since_match, _default)
+      when is_boolean(value) do
+    Map.put(struct, key, value)
+  end
+
+  def struct_set_boolean(struct, key, value, require, since_match, default)
+      when is_binary(value) do
+    case String.to_atom(value) do
+      bool_value when is_boolean(bool_value) ->
+        struct_set_boolean(struct, key, bool_value, require, since_match, default)
+
+      _ ->
+        raise "invalid boolean value #{value} when load #{struct.__struct__} struct for key #{key}"
+    end
+  end
+
+  def struct_set_boolean(struct, key, nil, @require_no, _since_match, default)
+      when is_boolean(default) or default == nil do
+    Map.put(struct, key, default)
+  end
+
+  def struct_set_boolean(struct, key, nil, @require_yes, @since_match_no, _default) do
+    struct
+  end
+
+  def struct_set_boolean(struct, key, nil, @require_yes, @since_match_yes, _default) do
+    raise "missing value for key #{key} when load #{struct.__struct__} struct"
+  end
+
+  # struct, key, value, require, version_match, default
+  def struct_set_decimal(struct, key, value, _require, _since_match, _default)
+      when Decimal.is_decimal(value) do
+    Map.put(struct, key, value)
+  end
+
+  def struct_set_decimal(struct, key, value, require, since_match, default)
+      when is_binary(value) do
+    case Decimal.parse(value) do
+      {decimal_value, ""} when Decimal.is_decimal(decimal_value) ->
+        struct_set_decimal(struct, key, decimal_value, require, since_match, default)
+
+      _ ->
+        raise "invalid decimal value #{value} when load #{struct.__struct__} struct for key #{key}"
+    end
+  end
+
+  def struct_set_decimal(struct, key, nil, @require_no, _since_match, default)
+      when  Decimal.is_decimal(default) or default == nil do
+    Map.put(struct, key, default)
+  end
+
+  def struct_set_decimal(struct, key, nil, @require_yes, @since_match_no, _default) do
+    struct
+  end
+
+  def struct_set_decimal(struct, key, nil, @require_yes, @since_match_yes, _default) do
+    raise "missing value for key #{key} when load #{struct.__struct__} struct"
   end
 
   # struct, key, value, require, version_match, default
