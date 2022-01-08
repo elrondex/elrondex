e5b062bae892648bcf5bb0f6297809345e486111
Elrondex <alchimistii.elixir@gmail.com>
2022-01-08 23:37:52 +0200

    Fix get_network_config tests

--- test/elrondex/rest_test.exs
+++ test/elrondex/rest_test.exs
@@ -5,29 +5,25 @@ defmodule Elrondex.RESTTest do
 
   test "test get_network_config on mainnet" do
     mainnet = Network.mainnet()
+    assert mainnet.erd_chain_id == "1"
     {:ok, config} = REST.get_network_config(mainnet)
-    # IO.inspect(config)    
     mainnet = Network.config(mainnet, config)
-    # IO.inspect(mainnet)    
     assert mainnet.erd_chain_id == "1"
   end
 
   test "test get_network_config on testnet" do
     testnet = Network.testnet()
+    assert testnet.erd_chain_id == "T"
     {:ok, config} = REST.get_network_config(testnet)
-    # IO.inspect(config)
     testnet = Network.config(testnet, config)
-    # IO.inspect(testnet)
     assert testnet.erd_chain_id == "T"
   end
 
   test "test get_network_config on devnet" do
     devnet = Network.devnet()
-    assert devnet.erd_chain_id == nil
+    assert devnet.erd_chain_id == "D"
     {:ok, config} = REST.get_network_config(devnet)
-    IO.inspect(config)
     devnet = Network.config(devnet, config)
-    IO.inspect(devnet)
     assert devnet.erd_chain_id == "D"
   end
 end
