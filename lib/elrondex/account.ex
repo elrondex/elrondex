224b83cc649343f17e546a3e3b6cd4c4e1ef8815
fabianflx <97527855+fabianflx@users.noreply.github.com>
2022-02-11 15:19:56 +0200

    Update account.ex

--- lib/elrondex/account.ex
+++ lib/elrondex/account.ex
@@ -62,7 +62,7 @@ defmodule Elrondex.Account do
       "erd1edmdkecu95u6aj9ehd0lf3d97qw85k86pkqqdu5029zcydslg7qs3tdc59"
 
       iex> Elrondex.Test.Bob.hex_private_key()
-      ...> |> Elrondex.Account.private_key_to_address()
+      ...> |> Elrondex.Account.from_private_key()
       "erd1edmdkecu95u6aj9ehd0lf3d97qw85k86pkqqdu5029zcydslg7qs3tdc59"
   """
   def from_private_key(private_key)
