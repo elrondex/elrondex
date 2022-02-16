157f7c1805dd2f6f0fd96e45e970b819ea9f9ec7
Hirjan Fabian <hirjanfabian@gmail.com>
2022-02-16 23:28:02 +0200

    test

--- lib/elrondex/account.ex
+++ lib/elrondex/account.ex
@@ -62,6 +62,7 @@ defmodule Elrondex.Account do
 
       iex> Elrondex.Test.Bob.hex_private_key()
       ...> |> Elrondex.Account.from_private_key()
+      ...> |> Map.get(:address)
       "erd1edmdkecu95u6aj9ehd0lf3d97qw85k86pkqqdu5029zcydslg7qs3tdc59"
   """
   def from_private_key(private_key)
