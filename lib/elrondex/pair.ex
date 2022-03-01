5929811d3a0f9b6275a8a14ae840124244c8eeed
Savu Victor <victor@fluxinternet.ro>
2022-03-01 12:11:32 +0200

    Added lp_token and lp_decimals

--- lib/elrondex/pair.ex
+++ lib/elrondex/pair.ex
@@ -6,16 +6,9 @@ defmodule Elrondex.Pair do
             first_token: nil,
             first_decimals: nil,
             second_token: nil,
-            second_decimals: nil
-
-
-  def get_egldbusd_pair do
-    %Pair{
-      address: "erd1qqqqqqqqqqqqqpgq3gmttefd840klya8smn7zeae402w2esw0n4sm8m04f",
-      first_token: "WEGLD-88600a",
-      second_token: "BUSD-05b16f"
-    }
-  end
+            second_decimals: nil,
+            lp_token: nil,
+            lp_decimals: nil
 
   def token_identifier(%Pair{first_token: identifier}, identifier) when is_binary(identifier) do
     identifier
